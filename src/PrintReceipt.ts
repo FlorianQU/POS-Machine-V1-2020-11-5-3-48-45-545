668e724c62e7264569cc4e80232800fd6cfdcf23
hockeyqqy <qqu@slb.com>
2020-11-18 00:45:05 +0800

    feat: pass test

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,12 +1,80 @@
-import {loadAllItems, loadPromotions} from './Dependencies'
+import { loadAllItems, loadPromotions } from './Dependencies'
 
 export function printReceipt(tags: string[]): string {
-  return `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
-**********************`
+  const allItems = loadAllItems()
+  const promotions = loadPromotions()
+  const itemList = tags.map(item => item.split('-')[0])
+  const uniqueItemList = [...new Set(itemList)]
+  const countList = generateCountList()
+  const receipt = generateReceipt()
+  return receipt
+
+  function generateCountList(): number[]{
+    const countList : number[] = new Array(uniqueItemList.length).fill(0)
+    tags.forEach(item => {
+      if (item.includes('-')) {
+        const splitResult = item.split('-')
+        countList[uniqueItemList.indexOf(splitResult[0])] += Number(splitResult[1])
+      }
+      else {
+        countList[uniqueItemList.indexOf(item)] += 1
+      }
+    })
+    return countList
+  }
+
+
+  function generateReceipt(): string{
+    let output = ''
+    output += '***<store earning no money>Receipt ***\n'
+    let actualsum = 0
+    let expectedsum = 0
+    allItems.forEach(item => {
+      const num = countList[uniqueItemList.indexOf(item.barcode)]
+      if (num > 0) {
+        output += `Name：${item.name}，`
+        output += `Quantity：${num} ${item.unit === 'a' ? '' : item.unit}s，`
+        output += `Unit：${item.price.toFixed(2)}(yuan)，`
+        output += `Subtotal：${getPrice(item).toFixed(2)}(yuan)\n`
+        actualsum += getPrice(item)
+        expectedsum += Number((item.price * num).toFixed(2))
+      }
+    })
+    const discount = expectedsum - actualsum
+    output += '----------------------\n'
+    output += `Total：${actualsum.toFixed(2)}(yuan)\n`
+    if(discount > 0){
+      output += `Discounted prices：${discount.toFixed(2)}(yuan)\n`
+    }
+    output += '**********************'
+    return output
+  }
+
+
+  function getPrice(item: Item): number {
+
+    const itemNum = countList[uniqueItemList.indexOf(item.barcode)]
+    if (promotions[0].barcodes.includes(item.barcode)) {
+
+      const actualNum = Math.floor(itemNum / 3) * 2 + itemNum % 3
+      // console.log(actualNum)
+      return item.price * actualNum
+    }
+    else return item.price * itemNum
+  }
+
+}
+
+
+class Item {
+  barcode: string
+  name: string
+  unit: string
+  price: number
+  constructor(barcode: string, name: string, unit: string, price: number) {
+    this.barcode = barcode
+    this.name = name
+    this.unit = unit
+    this.price = price
+  }
 }
